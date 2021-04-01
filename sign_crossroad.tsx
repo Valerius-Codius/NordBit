12cb4cdfbbc480ed5bd628835921fca6636a8981
Valentin Zeidler <valentin.zeidler@lmbit.de>
2021-04-01 13:22:48 +0200

    deleted crossroadssign

--- sign_crossroad.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="sign_crossroad" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="sign_crossroad.svg" width="128" height="128"/>
-</tileset>
